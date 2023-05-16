d0bb241f67784c5d99a6f3f33a794da8c82c3945
feiria <1597826520@qq.com>
2023-05-16 19:32:46 +0800

    docs: add pressure test report from jmeter

--- middleware/cors.go
+++ middleware/cors.go
@@ -3,23 +3,55 @@ package middleware
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"time"
+	"go-api/pkg/config"
+	"net/http"
+)
+
+var (
+	//todo 可在配置文件读取允许跨域列表
+	allowOrigins = []string{
+		"http://127.0.0.1:8867",
+		"https://127.0.0.1:8867",
+		"http://127.0.0.1:8868",
+		"https://127.0.0.1:8868",
+	}
+
+	allowMethods = []string{
+		http.MethodGet,
+		http.MethodPost,
+		http.MethodPut,
+		http.MethodDelete,
+		http.MethodOptions,
+
+		http.MethodPatch, // RFC 5789
+	}
+
+	allowHeaders = []string{
+		"Content-Type",
+		"Access-Token",
+		"Authorization",
+	}
+
+	exposeHeaders = []string{
+		"Content-Length",
+	}
 )
 
 func Cors() gin.HandlerFunc {
 	c := cors.Config{
-		// AllowAllOrigins: true,
-		// 接受哪些域名的请求
-		AllowOrigins:  []string{"http://localhost:8867"},
-		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "PATCH"},
-		AllowHeaders:  []string{"Content-Type", "Access-Token", "Authorization"},
-		ExposeHeaders: []string{"Content-Length"},
-		// 是否允许发送Cookie
+		AllowMethods: allowMethods,
+		AllowHeaders: allowHeaders,
+		//安全的头参数
+		ExposeHeaders: exposeHeaders,
+		//请求是否包含用户凭证
 		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return origin == "https://github.com"
-		},
-		MaxAge: 6 * time.Hour,
+	}
+
+	//开启debug则默认不阻止任何跨域请求
+	if config.Configs.Server.Debug {
+		c.AllowAllOrigins = true
+	} else {
+		c.AllowOrigins = allowOrigins
 	}
 
 	return cors.New(c)
